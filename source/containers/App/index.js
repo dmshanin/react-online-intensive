a825a8002c3fa14a191ea1aeaef2363c285b1f0c
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2018-09-28 01:59:43 +0300

    Урок 4. Контекст

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 // Components
 import Feed from 'components/Feed';
+import { Provider } from 'components/HOC/withProfiler';
 
 // Instrumens
 import avatar from 'theme/assets/lisa';
@@ -15,6 +16,10 @@ const options = {
 
 export default class App extends Component {
     render () {
-        return <Feed { ...options } />;
+        return (
+            <Provider value = { options } >
+                <Feed />
+            </Provider>
+        );
     }
 }
