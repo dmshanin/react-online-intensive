1e6e169954b5e892a75c3ae2d9df7ccecd45ff29
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2018-10-16 17:32:03 +0300

    Урок 9. Низкоуровневый Transition:

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,6 +1,8 @@
 // Core
 import React, { Component } from 'react';
 import cx from 'classnames';
+import { Transition } from 'react-transition-group';
+import { fromTo } from 'gsap';
 
 // Components
 import { withProfile } from 'components/HOC/withProfile';
@@ -34,6 +36,15 @@ export default class StatusBar extends Component {
         socket.removeListener('disconnect');
     }
 
+    _animateStatusBarEnter = (composer) => {
+        fromTo(
+            composer,
+            1,
+            { opacity: 0 },
+            { opacity: 1 }
+        );
+    };
+
     render () {
         const {
             avatar,
@@ -50,18 +61,24 @@ export default class StatusBar extends Component {
         const statusMessage = online ? 'Online' : 'Offline';
 
         return (
-            <section className = { Styles.statusBar }>
-                <div className = { statusStyle }>
-                    <div>{statusMessage}</div>
-                    <span />
-                </div>
-                <button>
-                    <img src = { avatar } />
-                    <span>{ currentUserFirstName }</span>
-                    &nbsp;
-                    <span>{ currentUserLastName }</span>
-                </button>
-            </section>
+            <Transition
+                appear
+                in
+                onEnter = { this._animateStatusBarEnter }
+                timeout = { 1000 }>
+                <section className = { Styles.statusBar }>
+                    <div className = { statusStyle }>
+                        <div>{statusMessage}</div>
+                        <span />
+                    </div>
+                    <button>
+                        <img src = { avatar } />
+                        <span>{ currentUserFirstName }</span>
+                        &nbsp;
+                        <span>{ currentUserLastName }</span>
+                    </button>
+                </section>
+            </Transition>
         );
     }
 }
