83124479ac864bedeab059f51384fb94ffa7e8e1
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2018-10-16 16:47:42 +0300

    Домашка

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -52,11 +52,25 @@ export default class Feed extends Component {
                 }));
             }
         });
+
+        socket.on('like', (postJSON) => {
+            const { data: likedPost, meta } = JSON.parse(postJSON);
+
+            if (
+                `${currentUserFirstName} ${currentUserLastName}` !==
+                `${meta.authorFirstName} ${meta.authorLastName}`
+            ) {
+                this.setState(({ posts }) => ({
+                    posts: posts.filter((post) => post.id !== likedPost.id),
+                }));
+            }
+        });
     }
 
     componentWillUnmount () {
         socket.removeListener('create');
         socket.removeListener('remove');
+        socket.removeListener('like');
     }
 
     _setPostsFetchingState = (state) => {
