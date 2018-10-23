a6c73d6fe0041d39124c379f905fe62eb4beb84f
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2018-10-23 22:44:45 +0300

    фиксы

--- source/components/Postman/index.js
+++ source/components/Postman/index.js
@@ -5,13 +5,11 @@ import React from 'react';
 import Styles from './styles.m.css';
 import { withProfile } from "../HOC/withProfile";
 
-const Postman = (props) => {
-    return (
-        <section className = { Styles.postman }>
-            <img src = { props.avatar } />
-            <span>Welcome online, {props.currentUserFirstName}</span>
-        </section>
-    );
-};
+const Postman = (props) => (
+    <section className = { Styles.postman }>
+        <img src = { props.avatar } />
+        <span>Welcome online, {props.currentUserFirstName}</span>
+    </section>
+);
 
 export default withProfile(Postman);
