6a7b0ac16897e659fe6a7e4b17a49f4206af51df
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2018-12-19 01:01:16 +0300

    Урок 13. React-Router

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -1,10 +1,13 @@
 // Core
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 // Components
 import Catcher from 'components/Catcher';
+import StatusBar from 'components/StatusBar';
 import Feed from 'components/Feed';
+import Profile from 'components/Profile';
 import { Provider } from 'components/HOC/withProfile';
 
 // Instruments
@@ -18,11 +21,24 @@ const options = {
 
 @hot(module)
 export default class App extends Component {
+    state = {
+
+    };
+
+    componentDidMount () {
+
+    }
+
     render () {
         return (
             <Catcher>
                 <Provider value = { options } >
-                    <Feed />
+                    <StatusBar />
+                    <Switch>
+                        <Route component = { Feed } path = '/feed' />
+                        <Route component = { Profile } path = '/profile' />
+                        <Redirect to = '/feed' />
+                    </Switch>
                 </Provider>
             </Catcher>
         );
